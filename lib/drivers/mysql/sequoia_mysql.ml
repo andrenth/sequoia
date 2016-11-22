6df6469003bc014bcf14029f1fa52062c010e7c7
Andre Nathan <andre@digirati.com.br>
2016-11-22 13:58:14 -0200

    Add missing case for Datetime

--- lib/drivers/mysql/sequoia_mysql.ml
+++ lib/drivers/mysql/sequoia_mysql.ml
@@ -671,6 +671,8 @@ module Select = struct
             { repr = Field.to_string fld; params = []; pos = st.pos }
         | Field (Field.Date _ as fld, _, _) ->
             { repr = Field.to_string fld; params = []; pos = st.pos }
+        | Field (Field.Datetime _ as fld, _, _) ->
+            { repr = Field.to_string fld; params = []; pos = st.pos }
         | Field (Field.Enum _ as fld, _, _) ->
             { repr = Field.to_string fld; params = []; pos = st.pos }
         (* Handover *)
